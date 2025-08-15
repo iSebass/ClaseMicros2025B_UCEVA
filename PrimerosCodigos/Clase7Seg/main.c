44ca0dc1a32ab047cb54ea6fbfc8bda9249e2aab
iSebas <jusecofe@gmail.com>
2025-08-15 11:25:20 -0500

    Se atienden dudas del ejercicio 02

--- PrimerosCodigos/Clase7Seg/main.c
+++ PrimerosCodigos/Clase7Seg/main.c
@@ -4,19 +4,28 @@
 
 #include "SEVENSEG/SEVENSEG_LIB.h"
 
-char contador=0; // 0 - 9
+#define DP_DECENAS  (1<<PINB0)
+#define DP_UNIDADES (1<<PINB1)
+
+char contador=35; // 00 - 99
+
+#define te 5
+
 
 int main(void)
 {
 	diplayInit();
+	DDRB |= (1<<PINB0)|(1<<PINB1);
 	//LOOP
     while (1) 
     {
-		displayDecoCC(contador);
-		_delay_ms(500);
-		contador++;
-	    contador = contador>9 ? 0:contador;
+		PORTB = ~DP_DECENAS;
+		displayDecoCC(3);
+		_delay_ms(te);
 		
+		PORTB = ~DP_UNIDADES;
+		displayDecoCC(5);
+		_delay_ms(te);
 		
     }
 }
